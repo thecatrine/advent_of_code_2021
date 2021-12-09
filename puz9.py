eb617936ea5a753e48759749e98970ffadca7048
Catherine David <miscatulated@gmail.com>
2021-12-08 21:44:49 -0800

    code golf

--- puz9.py
+++ puz9.py
@@ -11,41 +11,25 @@ for y in range(0,len(lines)):
         map[(y,x)] = num
 
 
-expanding = 11
-risks = 0
-low_risks = []
-for y in range(0,len(lines)):
-    for x in range(0,len(lines[0])):
-        a = map.get((y-1, x), 10)
-        b = map.get((y+1, x), 10)
-        c = map.get((y, x-1), 10)
-        d = map.get((y, x+1), 10)
-
-        e = map[(y,x)]
-        if e < min([a,b,c,d]):
-            print(f"low risk at {y},{x}")
-
-            #map[(y,x)] = expanding
-            low_risks.append((y,x))
-            expanding += 1
 
 
-def count(map, tup):
-    spots = set([tup])
-    count = 0
-    while not len(spots) == 0:
-        spot = spots.pop()
-        map[spot] = None
-        count += 1
-        y,x = spot
-        for new_spot in [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]:
-            if map.get(new_spot, None) not in [None, 9]:
-                spots |= set([new_spot])
+def tups(y,x):
+    return [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]
+
+def count(map, spots, count):
+    while spots:
+        y,x = spots.pop(); map[(y,x)] = 9; count += 1
+        [spots.add(new_spot) for new_spot in tups(y,x) if map.get(new_spot, 9) != 9]
     return count
 
+low_risks = []
+for y in range(len(lines)):
+    for x in range(len(lines[0])):
+        if map[(y,x)] < min([map.get(tup, 10) for tup in tups(y,x)]):
+            low_risks.append(count(map, set([(y,x)]), 0))
 
 import functools
-print(functools.reduce(lambda x,y: x*y, sorted([count(map, x) for x in low_risks])[-3:] ))
+print(functools.reduce(lambda x,y: x*y, sorted(low_risks)[-3:] ))
 
 #changing = True
 #while changing:
